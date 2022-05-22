aaf0d578706fcb9015dc62287718177f2d6c01fc
CaioCintra <caiocintra@alunos.utfpr.edu.br>
2022-05-22 20:04:06 -0300

    I-20: Upload de arquivo

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -1,5 +1,34 @@
-const app = require('./config/express');
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
 const port = app.get('port');
+
+app.set('view engine', 'ejs');
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"uploads/")
+    },
+    filename:function(req,file,cb){
+        cb(null, file.originalname + Date.now() + path.extname(file.originalname));
+    }
+})
+
+const upload = multer({storage});
+
+
+app.get("/",(req,res) => {
+    res.render("index");
+})
+
+app.post("/upload", upload.single("file"), (req,res)=> {
+    res.send("Arquivo recebido");
+})
+
+app.listen(8080,() => {
+    console.log("Rodando")
+});
 
 app.listen(port, ()=> {
     console.log(`aplicação rodando em http://localhost:${port}`);
