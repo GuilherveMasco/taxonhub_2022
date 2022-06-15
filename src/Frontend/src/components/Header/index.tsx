d1409f330074372a27940b7bfbf59b5397e7b25d
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-06-15 20:08:28 -0300

    botão select pronto

--- src/Frontend/src/components/Header/index.tsx
+++ src/Frontend/src/components/Header/index.tsx
@@ -1,14 +1,20 @@
 // aqui sera feito o header colocando os componentes necessario já criados
 
-import { Stack } from "@chakra-ui/react";
+import { HStack, Stack } from "@chakra-ui/react";
 import React from "react";
+import { Select } from "../Select/select";
 
 function Header() {
     return (
     
         <>
         <Stack className="bg-HeaderColor w-full h-182">
-
+            <HStack  className='m-14' spacing='5rem'>
+                <Select haslabel label='Tipo de busca'  w='w-72' h='h-16' fontSize='text-xl'>
+                    <option>Taxonômica</option>
+                    <option>Ocorrência</option>
+                </Select>
+            </HStack>
         </Stack>
         </>
       
