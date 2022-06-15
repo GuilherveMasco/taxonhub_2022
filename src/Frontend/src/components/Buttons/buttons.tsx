ad324b2b46a974bebe7a8bc4d5cf2b7867539eb9
Gaby <44858638+GabrielaMarangoni@users.noreply.github.com>
2022-06-14 23:10:10 -0300

    Revert "Botão de salvar o arquivo gerado (CSV) #59"

--- src/Frontend/src/components/Buttons/buttons.tsx
+++ src/Frontend/src/components/Buttons/buttons.tsx
@@ -4,27 +4,14 @@
 //exemplo (altere como necessario, nao vou dar feito)
 
 import React from "react";
-import { Button } from '@chakra-ui/react'
-import { MdFileUpload } from "react-icons/md"
 
-function BtnBuscarLista() {
+function Buttons() {
     return (
     
         <>
-            <b>Buscar Lista</b><br/>
-            <Button className=""
-                height='4rem'
-                width='18.75rem'
-                border='0.125rem'
-                fontSize={20}
-                style={{alignSelf: 'flex-end'}}
-                rightIcon={<MdFileUpload/>}
-                >
-                Enviar arquivo csv
-            </Button>
         </>
       
     );
 }
 
-export {BtnBuscarLista}
+export {Buttons}
