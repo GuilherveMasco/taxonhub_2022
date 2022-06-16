72e8cc75e10f2149b461d5705758cbc04bd9c064
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-06-15 21:23:48 -0300

    botao de salvar o arquivo criado

--- src/Frontend/src/components/Header/index.tsx
+++ src/Frontend/src/components/Header/index.tsx
@@ -2,26 +2,14 @@
 
 import { Stack } from "@chakra-ui/react";
 import React from "react";
-import { BtnBuscarLista } from "../Buttons/buttons";
+
 
 function Header() {
     return (
     
         <>
         <Stack className="bg-HeaderColor w-full h-182">
-            <br/>
-            <table>
-            <thead>
-            <tr>
-                <td width={50}></td>
-                <td></td>
-                <td><BtnBuscarLista/></td>
-                <td width={300}></td>
-                <td></td>
-                <td></td>
-            </tr>
-            </thead>
-            </table>
+            
         </Stack>
         </>
       
