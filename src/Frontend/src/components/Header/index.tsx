0567532d5f98820d9b17f6d947b036e2a99eacbe
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-06-15 21:29:44 -0300

    criando botao de pesquisar

--- src/Frontend/src/components/Header/index.tsx
+++ src/Frontend/src/components/Header/index.tsx
@@ -4,10 +4,10 @@ import { HStack, Stack } from "@chakra-ui/react";
 import React from "react";
 import { Buttons } from "../Buttons/buttons";
 import { TbFileUpload } from "react-icons/tb";
-
+import { FcSearch } from "react-icons/fc";
 
 function Header() {
-    return (    
+    return (
         <Stack className="bg-HeaderColor w-full h-182">
            <HStack className='m-14' spacing='5rem'>
                 <Buttons 
@@ -22,8 +22,17 @@ function Header() {
                     >
                     arquivo.csv <TbFileUpload size='2.5rem' />
                 </Buttons>
+
+                <Buttons 
+                    rounded='rounded-xl' 
+                    w='w-20' 
+                    h='h-16'
+                    >
+                     <FcSearch size='3.5rem' />
+                </Buttons>
+
            </HStack>
-        </Stack>     
+        </Stack>      
       
     );
 }
