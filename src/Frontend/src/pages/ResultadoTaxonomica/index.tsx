481be0708276df6de1974ae280bcbd9262e9294d
CaioCintra <caiocintra@alunos.utfpr.edu.br>
2022-06-16 23:10:53 -0300

    Merge

--- src/Frontend/src/pages/ResultadoTaxonomica/index.tsx
+++ src/Frontend/src/pages/ResultadoTaxonomica/index.tsx
@@ -1,15 +1,8 @@
 import { Flex, Table, TableContainer, Tbody, Th, Thead, Tr, VStack } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
-import { Buttons } from '../../components/Buttons/buttons';
 import { Header } from '../../components/Header';
-<<<<<<< HEAD
-import { ComponentsTable } from '../../components/SortingTable/componentsTable';
-import { ISorting } from '../../models/sorting';
-import { RiSave3Fill } from "react-icons/ri";
-=======
 import { ComponentsTable } from '../../components/TaxonomicTable/componentsTable';
 import { ITaxonomic } from '../../models/taxonomic';
->>>>>>> 6d38db1b56c446b0a2a3f5e938ef000a96ef592c
 
 export default function ResultadoTaxonomico() {  
     const [taxonomic, setTaxonomic] = useState<ITaxonomic[]>([] as ITaxonomic[]);
@@ -76,7 +69,7 @@ export default function ResultadoTaxonomico() {
                             </h1>
                         </div>
 
-                        <div className=" bg-white w-1990 h-647 rounded-3xl flex flex-row">
+                        <div className=" bg-white w-1990 h-712 rounded-3xl flex flex-row">
                             <TableContainer className="w-full h-full">
                                 <Table variant='striped' colorScheme='teal'>
                                     <Thead>
@@ -96,15 +89,7 @@ export default function ResultadoTaxonomico() {
                                     </Tbody>      
                                 </Table>
                             </TableContainer>                     
-                        </div>  
-                        <div className="flex flex-row-reverse">
-                            <div >
-                                 <Buttons rounded='rounded-xl' text='text-xl' >
-                                    Salvar arquivo gerado <RiSave3Fill size='2.5rem'/>
-                                </Buttons>        
-
-                            </div>
-                        </div>         
+                        </div>                        
                     </VStack>
                 </div>
             </Flex>               
