2b1544ff9b922f49964a2baafd2c5242bbe04fc1
CaioCintra <caiocintra@alunos.utfpr.edu.br>
2022-06-18 14:44:18 -0300

    Modal Loading

--- src/Frontend/src/components/Header/index.tsx
+++ src/Frontend/src/components/Header/index.tsx
@@ -1,65 +1,107 @@
 // aqui sera feito o header colocando os componentes necessario já criados
 
-import { HStack, Stack, Box } from "@chakra-ui/react";
+import { HStack, Stack, Box, Image, useDisclosure, Button, Text, Spinner, Center } from "@chakra-ui/react";
 import { Buttons } from "../Buttons/buttons";
 import { TbFileUpload } from "react-icons/tb";
 import React, { useState } from "react";
 import { Select } from "../Select/select";
 import { MdSearch } from "react-icons/md";
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
 
 export function Header() {
     const [type, setType] = useState();
+
+    
+    const OverlayOne = () => (
+        <ModalOverlay
+        bg='blackAlpha.300'
+        backdropFilter='blur(10px)'
+        />
+        )
+        
+        const { isOpen, onOpen, onClose } = useDisclosure()
+        const [overlay, setOverlay] = React.useState(<OverlayOne />)
 
     function onChangeType(e: React.ChangeEvent){        
         const target = e.target as HTMLSelectElement
         console.log(target?.value);
     }
     return (
-        <Stack className="bg-HeaderColor w-full h-182">
-            <form>
-            <HStack className='m-14' spacing='5rem'>
-            <Buttons
-                    text='text-xl' 
-                    rounded='rounded-xl' 
-                    w='w-72' 
-                    h='h-16'
-                    wMin='min-w-72' 
-                    hMin='min-h-16'
-                    wMax='max-w-72'
-                    hMax='max-h-16'
-                    disabled
-                    >
-                    Enviar arquivo
-                    <TbFileUpload size='3rem' color='transparent'/>
-                    <Box display="inherit" color='transparent' overflow='hidden' opacity={1}>
-                    <input type="file" accept=".csv" id='fileInput' required/>
-                    </Box>
-            </Buttons>
-
-                <Select
-                w='w-72' 
-                h='h-16' 
-                fontSize='text-xl'                
-                value={type}  
-                id='select'
-                required
-                onChange={(e: React.ChangeEvent) => onChangeType(e)}
+        <Stack className="bg-HeaderColor w-full h-40">
+            <form 
+            onSubmit={() => {
+                setOverlay(<OverlayOne />)
+                onOpen()
+                }}
                 >
-                    <option value='taxonomic'>Taxonômica</option>
-                    <option value='occurrence'>Ocorrência</option>
-                </Select>
-                
-                <Buttons 
-                    rounded='rounded-xl' 
-                    w='w-20' 
-                    h='h-16'
-                    id='submit'
-                    type="submit"
+            <HStack className='m-10 px-8' spacing='56rem' >
+                    <Image src="https://i.ibb.co/6y78TjR/logo.png" alt="Logo" width={277} height={86} />
+                <HStack spacing='5rem'>
+                    <Buttons
+                        w='w-72' 
+                        h='h-16'                    
                     >
-                     <MdSearch size='3.5rem' />
-                </Buttons>
+                        Enviar arquivo
+                        <TbFileUpload size='3rem' color='transparent'/> {/* É gambiarra mesmo, não sei um jeito melhor */}
+                        <Box display="inherit" color='transparent' overflow='hidden' opacity={1}>
+                            <input type="file" accept=".csv" id='fileInput' required/>
+                        </Box>
+
+                    </Buttons>
+                
+                    <Select
+                        w='w-72' 
+                        h='h-16'                                    
+                        value={type}  
+                        id='select'
+                        required
+                        onChange={(e: React.ChangeEvent) => onChangeType(e)}
+                    >
+                        <option value='taxonomic'>Taxonômica</option>
+                        <option value='occurrence'>Ocorrência</option>
+                    </Select>
+                    
+                    <Buttons                     
+                        w='w-20' 
+                        h='h-16'
+                        id='submit'
+                        type="submit"
+                    >
+                        <MdSearch size='3.5rem' />
+                    </Buttons>
+                </HStack>
             </HStack>
-                </form>
+            </form>
+            <Modal isCentered isOpen={isOpen} onClose={onClose}>
+            {overlay}
+            <ModalContent>
+            <ModalHeader>Carregando ...</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                <Center>
+            <Spinner
+                thickness='8px'
+                speed='0.65s'
+                emptyColor='gray.200'
+                color='green.500'
+                size='xl'
+                />
+                </Center>
+            </ModalBody>
+            <ModalFooter>
+                <Button onClick={onClose}>Cancelar</Button>
+            </ModalFooter>
+            </ModalContent>
+            </Modal>
         </Stack>
+
     );
 }
