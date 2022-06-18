b063d3a8c2c4d75c56d2faffd17e18ddc33bebdd
Vanakaus <vinicius_vnk@hotmail.com>
2022-06-17 21:23:38 -0300

    Adicionando rota para salvar o CSV de Ocorrencias num diretorio local

--- src/Backend/api/modules/uses/services/saveCSV/saveCSVController.js
+++ src/Backend/api/modules/uses/services/saveCSV/saveCSVController.js
@@ -1,11 +1,11 @@
 const papa = require("papaparse");
+const http = require('node:http');
 const fs = require('fs');
-const { listFDBAzurea } = require("../../../../../tests/testsFDB");
 
 module.exports = () => {
   const controller = {};
 
-  controller.saveCSV = async (req, res) => {
+  controller.saveCSVTaxonomica = async (req, res) => {
     console.log();
     console.log("Salvando o CSV...");
     console.log();
@@ -20,8 +20,59 @@ module.exports = () => {
       }
       res.send("Arquivo salvo com sucesso!");
     });
-
     
+  };
+
+
+  controller.saveCSVOcorrencias = async (req, res) => {
+    const caminho = req.body.caminho;
+
+    console.log();
+    console.log("Salvando o CSV de Ocorrencias em:");
+    console.log(caminho);
+    console.log();
+
+    const csvOcorrencias = JSON.stringify({ "names" : ["Eichhornia azurea (Sw.) Kunth"] });
+    var csv = "";
+    
+
+    const getCSV = http.request(
+      {
+        hostname: 'localhost',
+        port: 8080,
+        path: '/specieslink',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(csvOcorrencias)
+        }}, (resp) => {
+
+          resp.setEncoding('utf8');
+
+          resp.on('data', (chunk) => {
+            csv += chunk;
+          });
+          
+          resp.on('end', () => {
+            csv = papa.unparse(csv);
+            fs.writeFile(caminho, csv, err => {
+              if (err) {
+                console.error(err);
+                res.send("ERRO Ao salvar o arquivo!");
+                console.log("ERRO Ao salvar o arquivo!");
+              }
+              res.send("Arquivo salvo com sucesso!");
+              console.log("Arquivo salvo com sucesso!");
+            });
+          });
+        });
+          
+        getCSV.on('error', (e) => {
+          console.error(`problem with request: ${e.message}`);
+        });
+
+        getCSV.write(csvOcorrencias);
+        getCSV.end();
   };
   return controller;
 };
