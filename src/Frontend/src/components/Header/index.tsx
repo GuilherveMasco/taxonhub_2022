ea4329b83ab18de3c55117be6e5f5a74a348d80a
Gaby <44858638+GabrielaMarangoni@users.noreply.github.com>
2022-06-17 22:18:03 -0300

    Revert "Botao pesquisar #69"

--- src/Frontend/src/components/Header/index.tsx
+++ src/Frontend/src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 // aqui sera feito o header colocando os componentes necessario já criados
 
-import { HStack, Stack, Box } from "@chakra-ui/react";
+import { HStack, Stack, FormControl, Box } from "@chakra-ui/react";
 import { Buttons } from "../Buttons/buttons";
 import { TbFileUpload } from "react-icons/tb";
 import React, { useState } from "react";
 import { Select } from "../Select/select";
-import { MdSearch } from "react-icons/md";
 
 export function Header() {
     const [type, setType] = useState();
+
 
     function onChangeType(e: React.ChangeEvent){        
         const target = e.target as HTMLSelectElement
@@ -16,7 +16,6 @@ export function Header() {
     }
     return (
         <Stack className="bg-HeaderColor w-full h-182">
-            <form>
             <HStack className='m-14' spacing='5rem'>
             <Buttons
                     text='text-xl' 
@@ -27,39 +26,29 @@ export function Header() {
                     hMin='min-h-16'
                     wMax='max-w-72'
                     hMax='max-h-16'
-                    disabled
                     >
                     Enviar arquivo
-                    <TbFileUpload size='3rem' color='transparent'/>
+                    <TbFileUpload size='3rem' color='transparent'/> {/* É gambiarra mesmo, não sei um jeito melhor */}
                     <Box display="inherit" color='transparent' overflow='hidden' opacity={1}>
-                    <input type="file" accept=".csv" id='fileInput' required/>
+                    <input type="file" accept=".csv" id='fileInput'/>
                     </Box>
             </Buttons>
 
-                <Select
+
+
+                <Select 
                 w='w-72' 
                 h='h-16' 
                 fontSize='text-xl'                
                 value={type}  
-                id='select'
-                required
+                
                 onChange={(e: React.ChangeEvent) => onChangeType(e)}
                 >
+                    <option value='undefined' >Tipo de busca</option>
                     <option value='taxonomic'>Taxonômica</option>
                     <option value='occurrence'>Ocorrência</option>
                 </Select>
-                
-                <Buttons 
-                    rounded='rounded-xl' 
-                    w='w-20' 
-                    h='h-16'
-                    id='submit'
-                    type="submit"
-                    >
-                     <MdSearch size='3.5rem' />
-                </Buttons>
             </HStack>
-                </form>
         </Stack>
     );
 }
--- src/Frontend/src/pages/ResultadoTaxonomica/index.tsx
+++ src/Frontend/src/pages/ResultadoTaxonomica/index.tsx
@@ -1,10 +1,8 @@
 import { Flex, Table, TableContainer, Tbody, Th, Thead, Tr, VStack } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
-import { RiSave3Fill } from "react-icons/ri";
 import { ComponentsTable } from '../../components/TaxonomicTable/componentsTable';
 import { ITaxonomic } from '../../models/taxonomic';
-
 
 export default function ResultadoTaxonomico() {  
     const [taxonomic, setTaxonomic] = useState<ITaxonomic[]>([] as ITaxonomic[]);
